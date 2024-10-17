1804ea72ca2a108de614af154e8ff1e86873351f
HamzaDazai <hamzadazai6@gmail.com>
2024-10-17 19:39:50 +0100

    new push

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include "libft.h"
-#include <ctype.h>
 
 int main(void)
 {
-    for (int i = 0; i < 128; i++)
-    {
-        if (ft_isprint(i))
-            printf("'%c' (ASCII: %d) is printable.\n", i, i);
-        else
-            printf("ASCII: %d is not printable.\n", i);
-    }
+    char *test1 = "Hello, world!";
+    char *test2 = "42 School";
+    char *test3 = "";
+    char *test4 = "C programming";
+
+    printf("Length of '%s': %d\n", test1, ft_strlen(test1));
+    printf("Length of '%s': %d\n", test2, ft_strlen(test2));
+    printf("Length of '%s': %d\n", test3, ft_strlen(test3));
+    printf("Length of '%s': %d\n", test4, ft_strlen(test4));
+
     return 0;
 }
