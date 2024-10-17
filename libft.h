4f4cc8af7813347db514e9c3c139782da183ed90
HamzaDazai <hamzadazai6@gmail.com>
2024-10-17 18:35:01 +0100

    update all

--- libft.h
+++ libft.h
@@ -3,5 +3,6 @@
 #include <unistd.h>
 
 int ft_isalpha(int c);
+int ft_isdigit(int c);
 
 #endif
--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include "libft.h"
 
 int main() {
-    char str[] = "hamzad42";
+    char str[] = "ham22da2";
     int i = 0;
 
     while (str[i] != '\0') {
         if (ft_isalpha(str[i])) {
             write(1, &str[i], 1);
+        }
+        else if (ft_isdigit(str[i]))
+        {
+            write(1,"g", 1);
         }
         i++;
     }
