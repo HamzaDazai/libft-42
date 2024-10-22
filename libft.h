ce02bca66193e3e6e81bc140a4c96acc720a6662
HamzaDazai <hamzadazai6@gmail.com>
2024-10-22 21:29:18 +0100

    push tester

--- libft.h
+++ libft.h
@@ -10,7 +10,7 @@ int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
 int ft_isprint(int c);
-size_t ft_strlen(char *str);
+size_t ft_strlen(const char *str);
 int ft_atoi(const char *str);
 int ft_toupper(int c);
 int ft_tolower(int c);
