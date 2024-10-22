1bc60646fa57e29f822c1fe80c24d4aaa0620594
HamzaDazAI <hamzadazia6@gmail.com>
2024-10-22 20:07:43 +0100

    add strlcpy

--- libft.h
+++ libft.h
@@ -4,17 +4,18 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int ft_isalpha(int  c);
-int ft_isdigit(int  c);
-int ft_isalnum(int  c);
-int ft_isascii(int  c);
-int ft_isprint(int  c);
-int ft_isprint(int  c);
-size_t ft_strlen(char  *str);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_isprint(int c);
+size_t ft_strlen(char *str);
 int ft_atoi(const char *str);
-int ft_toupper(int  c);
-int ft_tolower(int  c);
+int ft_toupper(int c);
+int ft_tolower(int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
-
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
+size_t ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
 
 #endif
--- main.c
+++ main.c
@@ -4,24 +4,25 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t i;
+  size_t i;
 
-    i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < (n -1))
-    {
-        i++;
-    }
-    return (s1[i] - s2[i]);
+  i = 0;
+  while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < (n - 1))
+  {
+    i++;
+  }
+  return (s1[i] - s2[i]);
 }
 
-int main() {
+int main()
+{
   char str1[] = "abcda", str2[] = "abcdb";
   int result;
-  int n = 4 ;
+  int n = 4;
   // comparing strings str1 and str2
   result = ft_strncmp(str1, str2, n);
   printf("ft_strcmp(str1, str2, and (n) ) = %d\n", result);
-  
+
   result = strncmp(str1, str2, n);
   printf("strncmp(str1, str2) = %d\n", result);
 
