ea8e08d29809af98464ecc4740afae071e654c0f
HamzaDazai <hamzadazai6@gmail.com>
2024-10-23 23:57:35 +0100

    fix normennite

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,27 +12,27 @@
 
 #include "libft.h"
 
-size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t  dst_len;
-    size_t  src_len;
-    size_t  i;
-    size_t  j;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
 
-    dst_len = ft_strlen(dst);
-    src_len = ft_strlen(src);
-    i = dst_len;
-    j = 0;
-    if (!dst || !src)
-        return (0);
-    if (dstsize <= dst_len)
-        return(dstsize + src_len);
-    while (src[j] != '\0' && i < (dstsize - 1))
-    {
-        dst[i] = src[j];
-        i++;
-        j++;
-    }
-    dst[i] = '\0';
-    return (dst_len + src_len);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = dst_len;
+	j = 0;
+	if (!dst || !src)
+		return (0);
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	while (src[j] != '\0' && i < (dstsize - 1))
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (dst_len + src_len);
 }
