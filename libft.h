a3316511c34252bf558a88cd7baa3726086a1228
HamzaDazai <hamzadazai6@gmail.com>
2024-10-24 00:02:19 +0100

    add

--- libft.h
+++ libft.h
@@ -30,6 +30,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
 size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 char * ft_strnstr(const char *haystack, const char *needle, size_t len);
-char * strchr(const char *s, int c);
-char * strchr(const char *s, int c);
+char * ft_stchr(const char *s, int c);
+char * ft_strrchr(const char *s, int c);
 #endif
