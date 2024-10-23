c19dabf43667cf6b6eb39b293873fd5aa2455cf0
HamzaDazAI <hamzadazia6@gmail.com>
2024-10-23 19:08:24 +0100

    anather fonction

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,31 +12,31 @@
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int sign;
-    long int rs;
+	int	i;
+	int	sign;
+	long int	rs;
 
-    i = 0;
-    sign = 1;
-    rs = 0;
-    while (str[i] != '\0' && ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32)))
-    {
-        i++;
-    }
-    if (str[i] == '+' || str[i] == '-')
-    {
-        if (str[i] == '-')
-        {
-            sign *= -1;
-        }
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        rs = rs * 10 + (str[i] - 48);
-        i++;
-    }
-    return (rs * sign);
+	i = 0;
+	sign = 1;
+	rs = 0;
+	while (str[i] != '\0' && ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32)))
+	{
+		i++;
+	}
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		rs = rs * 10 + (str[i] - 48);
+		i++;
+	}
+	return (rs * sign);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hdazia <hdazia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:50:29 by hdazia            #+#    #+#             */
-/*   Updated: 2024/10/23 15:50:32 by hdazia           ###   ########.fr       */
+/*   Updated: 2024/10/23 19:07:28 by hdazia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
 size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 char * ft_strnstr(const char *haystack, const char *needle, size_t len);
-
-
+char * strchr(const char *s, int c);
+char * strchr(const char *s, int c);
 #endif
