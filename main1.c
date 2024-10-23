66ed5b0eaaa2545f8d2599a81bb28c9d2cfa96dc
HamzaDazAI <hamzadazia6@gmail.com>
2024-10-23 10:47:07 +0100

    more update

--- main1.c
+++ main1.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-#include "libft.h"
 #include <string.h>
 
-int main()
-{
-  char str[] = "hamza ";
-  char dst[] = "is dazia";
-  int dstsize = 9;
-  int 
+int main() {
+    char dest[500] = "Hello hamza ";
+    const char *src = " Dazia!";
+    size_t dest_size = sizeof(dest);
+
+    // Concatenate src to dest with size limit
+    size_t result = strlcat(dest, src, dest_size);
+
+    // Print the result
+    printf("Concatenated string: %s\n", dest);
+    printf("Total length of the string tried to create: %zu\n", result);
+
+    return 0;
 }
