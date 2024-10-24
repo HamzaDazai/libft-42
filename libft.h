341683edd73cffb818a8c9577309a46bb03f7d57
HamzaDazai <hamzadazai6@gmail.com>
2024-10-24 17:25:10 +0100

    head

--- libft.h
+++ libft.h
@@ -31,4 +31,6 @@ size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize
 char * ft_strnstr(const char *haystack, const char *needle, size_t len);
 char * ft_strchr(const char *s, int c);
 char * ft_strrchr(const char *s, int c);
+void bzero(void *s, size_t n);
+
 #endif
