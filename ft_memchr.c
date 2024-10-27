20ceee0f1f1012f9cf6057e82a7509a060e1f971
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-10-27 15:33:13 +0100

    add ft_memchr and main tester

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hdazia <hdazia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 21:01:16 by hdazia            #+#    #+#             */
-/*   Updated: 2024/10/26 21:01:23 by hdazia           ###   ########.fr       */
+/*   Updated: 2024/10/27 15:27:41 by hdazia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void * ft_memchr(const void *s, int c, size_t n)
 {
+    unsigned char *p_s;
+    size_t  i;
     
+    i = 0;
+    p_s = (unsigned char *)s;
+    while(i < n)
+    {
+        if (p_s[i] == (unsigned char)c)
+            return (&p_s[i]);
+        i++;
+    }
+    return NULL;
 }
